805bbcc5e3e042b20dfabb2d82b9f4c0f0525cef
PatNikVoe <patrick.niklas.voelkl@gmial.com>
2018-11-05 15:50:49 +0100

    removed no longer neccessary functions

--- calculations.py
+++
@@ -1,9 +0,0 @@
-from main import *
-
-# Convert an string to float
-def convertStringToFloat(input):
-    # Check if entered with Comma -> replace if so
-    if input.find(",",0,len(input))>0:
-        input = input.replace(",",".")
-    # convert distance to float
-    return float(input)
