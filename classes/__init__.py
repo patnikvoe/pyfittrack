2e8260cf07abe0a03a56fc1c5c6041da3f60097d
PatNikVoe <patrick.niklas.voelkl@gmial.com>
2018-11-05 15:51:22 +0100

    adjusted init.py in classes

--- classes/__init__.py
+++ classes/__init__.py
@@ -1,14 +1,8 @@
 __all__ = [
-        'AlpineTrack',
-        'Country',
-        'Difficulty',
-        'Mountain',
-        'MountainType',
-        'RouteRun',
-        'Sport',
-        'TrackRun',
-        'User',
-        'Weight'
+        'classes',
+        'functions',
+        'plots',
+        'menues'
         ]
 
 from sqlalchemy import Column, create_engine, Integer, Sequence, DateTime, Text, Boolean, Float, ForeignKey, String, Date
@@ -33,7 +27,6 @@ tb_routes = "routes"
 tb_difficulty = "difficulty"
 tb_tracksrun = "tracksrun"
 tb_tracksalpine = "tracksalpine"
-tb_rungoals = "rungoals"
 tb_weight = "weight"
 tb_mtype = "mtype"
 tb_country = "country"
@@ -42,13 +35,11 @@ tb_users = "users"
 # id Names
 user_id = "user_id_seq"
 sport_id = "sport_id_seq"
-mountains_id = "mountains_id_seq"
-routes_id = "routes_id_seq"
+mountain_id = "mountains_id_seq"
+route_id = "routes_id_seq"
 difficulty_id = "difficulty_id_seq"
-tracksrun_id = "tracksrun_id_seq"
-tracksalpine_id = "tracksalpine_id_seq"
-rungoals_id = "rungoals_id_seq"
+trackrun_id = "tracksrun_id_seq"
+trackalpine_id = "tracksalpine_id_seq"
 weight_id = "weight_id_seq"
 mtype_id = "mtype_id_seq"
 country_id = "country_id_seq"
-users_id = "users_id_seq"
