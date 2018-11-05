1ec512d1cad22ee2b2457a0670a43093141caf37
PatNikVoe <patrick.niklas.voelkl@gmial.com>
2018-11-05 11:57:06 +0100

    id sequence names and pandas import added

--- classes/__init__.py
+++ classes/__init__.py
@@ -19,6 +19,7 @@ from sqlalchemy.ext.declarative import declarative_base
 import datetime
 import time
 from math import log10
+import pandas as pd
 
 Engine = create_engine("postgresql://pyfittracker:Fuss93ball6@localhost:5432/pyfittracker")
 Base = declarative_base()
@@ -37,3 +38,17 @@ tb_weight = "weight"
 tb_mtype = "mtype"
 tb_country = "country"
 tb_users = "users"
+
+# id Names
+user_id = "user_id_seq"
+sport_id = "sport_id_seq"
+mountains_id = "mountains_id_seq"
+routes_id = "routes_id_seq"
+difficulty_id = "difficulty_id_seq"
+tracksrun_id = "tracksrun_id_seq"
+tracksalpine_id = "tracksalpine_id_seq"
+rungoals_id = "rungoals_id_seq"
+weight_id = "weight_id_seq"
+mtype_id = "mtype_id_seq"
+country_id = "country_id_seq"
+users_id = "users_id_seq"
