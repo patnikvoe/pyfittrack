e79c53df2759137242d3cc9cff7c42dcf2b399ed
PatNikVoe <patrick.niklas.voelkl@gmial.com>
2018-11-05 15:51:01 +0100

    new tests

--- Test.py
+++ Test.py
@@ -1,21 +1,36 @@
 #!/usr/bin/python3.6
 
 # Import all classes
-from classes.AlpineTrack import AlpineTrack
-from classes.Country import Country
-from classes.Difficulty import Difficulty
-from classes.Mountain import Mountain
-from classes.MountainType import MountainType
-from classes.RouteRun import RouteRun
-from classes.Sport import Sport
-from classes.TrackRun import TrackRun
-from classes.User import User
-from classes.Weight import Weight
-
-from classes import Base, Engine, Session, session
+from classes.classes import *
+from classes.functions import moveToDatabase
+from classes import Base, Engine, session
 from sqlalchemy import *
-from dbInterfacing import moveToDatabase
+
+Base.metadata.create_all(Engine)
 
 
-Sport.new()
-newSport2 = Sport(name="Wandern")
+UID = 13
+
+for user in session.query(User).filter(User.id==UID):
+    pat = User(name=user.name,birthday=user.birthday,male =user.male,height=user.height)
+
+# cancel = True
+# while cancel:
+#     i = input("Continue Input? (y/n)")
+#
+#     if i == "y" or i == "Y" or i =="j" or i== "J":
+#         pat.newWeight()
+#     else:
+#         cancel = False
+
+
+
+
+for weight in session.query(Weight).filter(Weight.user_id == UID):
+    pat.weights.append(weight)
+
+
+# Accessing all weights of User
+for i in range(len(pat.weights)):
+    print(pat.weights[i].weight)
+    print(pat.weights[i].date)
