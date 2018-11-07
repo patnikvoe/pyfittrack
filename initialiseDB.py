e1bb07abdbee2a0ae3985fbd394f3e5b31f1443a
PatNikVoe <patrick.niklas.voelkl@gmial.com>
2018-11-07 12:20:49 +0100

    changed ' to "; added mountain Types

--- initialiseDB.py
+++ initialiseDB.py
@@ -7,30 +7,30 @@ from classes import Base, Engine, session
 from sqlalchemy import *
 
 Base.metadata.create_all(Engine)
-d= {"Sport": ['Skitour','Wandern', 'Klettern', 'Klettersteig', 'Schneeschuhwanderung','Hochtour','Skihochtour','Trailrunning','Eisklettern']}
+d= {"Sport": ["Skitour","Wandern", "Klettern", "Klettersteig", "Schneeschuhwanderung","Hochtour","Skihochtour","Trailrunning","Eisklettern"]}
 df = pd.DataFrame(d)
-df = df.sort_values(['Sport'], ascending=[True])
+df = df.sort_values(["Sport"], ascending=[True])
 for index, row in df.iterrows():
-    sport = Sport(name=row['Sport'])
+    sport = Sport(name=row["Sport"])
     session.add(sport)
 
 session.commit()
 
-session.add_all([Difficulty(code='S1',description="Skitouren bis 30 Grad, dazu können noch Gipfelanstiege kommen, die Trittsicherheit und Schwindelfreiheit erfordern. Bei der Abfahrt: Erste Tiefschneekenntnisse. Sie fahren kontrolliert Kurven auf allen Pisten in paralleler Skistellung.",sport_id='7'),
-Difficulty(code='S2',description='Skitouren bis 35 Grad, sicheres Beherrschen der Spitzkehrentechnik, Trittsicherheit und Schwindelfreiheit, eventuell braucht man die Hände fürs Gleichgewicht. Bei der Abfahrt: Sicheres Kurvenfahren im Tiefschnee auch im steileren und/oder unübersichtlichen Gelände in paralleler Skistellung, aus Sicherheitsgründen gelegentlich in der Spur des Fachübungsleiters.',sport_id='7'),
-Difficulty(code='S3',description='Skitouren über 35 Grad, sehr sicheres Beherrschen der Spitzkehrentechnik, Trittsicherheit und Schwindelfreiheit, dazu können noch Kletterpassagen bis/über II. Schwierigkeitsgrad kommen. Bei der Abfahrt: Beherrschen des Kurvenfahrens in paralleler Skistellung im Tiefschnee bei allen Schneearten, auch im steilen und sehr steilen Gelände, auch mit höherem Tempo und unterschiedlichen Radien. Dies gilt ebenso bei Freeride Touren extrem steilen Gelände.',sport_id='7'),
-Difficulty(code='W1',description='Wandern auf breiten Wegen und markierten Wanderpfaden, keine Absturzgefahr',sport_id='9'),
-Difficulty(code='W2',description='Wandern auf markierten Wanderpfaden, exponierten Stellen möglich, die Trittsicherheit und Schwindelfreiheit erfordern',sport_id='9'),
-Difficulty(code='W3',description='Wandern auf markierten Wanderpfaden, mit exponierten Stellen, die Trittsicherheit und Schwindelfreiheit erfordern, ausgesetzte Stellen können mit Seilen oder Ketten gesichert sein, eventuell braucht man die Hände fürs Gleichgewicht',sport_id='9'),
-Difficulty(code='B1',description='Begehen von markierten Steigen, Geröllflächen, weglosen Schrofen, Gelände bereits recht exponiert, erfordert Trittsicherheit und Schwindelfreiheit sowie Alpinklettern I. Schwierigkeitsgrad',sport_id='9'),
-Difficulty(code='B2',description='Begehen von markierten Steigen und weglosem Gelände, heikle Grashalden, steile Geröllflächen, einfache Firnfelder, erfordert Trittsicherheit und Schwindelfreiheit sowie Alpinklettern bis II. Schwierigkeitsgrad',sport_id='9'),
-Difficulty(code='B3',description='Begehen von meist weglosem Gelände, steilen Schrofen, Firnfelder mit Ausrutschgefahr, erfordert Trittsicherheit und Schwindelfreiheit sowie Alpinklettern über II. Schwierigkeitsgrad',sport_id='9')
+session.add_all([Difficulty(code="S1",description="Skitouren bis 30 Grad, dazu können noch Gipfelanstiege kommen, die Trittsicherheit und Schwindelfreiheit erfordern. Bei der Abfahrt: Erste Tiefschneekenntnisse. Sie fahren kontrolliert Kurven auf allen Pisten in paralleler Skistellung.",sport_id="7"),
+Difficulty(code="S2",description="Skitouren bis 35 Grad, sicheres Beherrschen der Spitzkehrentechnik, Trittsicherheit und Schwindelfreiheit, eventuell braucht man die Hände fürs Gleichgewicht. Bei der Abfahrt: Sicheres Kurvenfahren im Tiefschnee auch im steileren und/oder unübersichtlichen Gelände in paralleler Skistellung, aus Sicherheitsgründen gelegentlich in der Spur des Fachübungsleiters.",sport_id="7"),
+Difficulty(code="S3",description="Skitouren über 35 Grad, sehr sicheres Beherrschen der Spitzkehrentechnik, Trittsicherheit und Schwindelfreiheit, dazu können noch Kletterpassagen bis/über II. Schwierigkeitsgrad kommen. Bei der Abfahrt: Beherrschen des Kurvenfahrens in paralleler Skistellung im Tiefschnee bei allen Schneearten, auch im steilen und sehr steilen Gelände, auch mit höherem Tempo und unterschiedlichen Radien. Dies gilt ebenso bei Freeride Touren extrem steilen Gelände.",sport_id="7"),
+Difficulty(code="W1",description="Wandern auf breiten Wegen und markierten Wanderpfaden, keine Absturzgefahr",sport_id="9"),
+Difficulty(code="W2",description="Wandern auf markierten Wanderpfaden, exponierten Stellen möglich, die Trittsicherheit und Schwindelfreiheit erfordern",sport_id="9"),
+Difficulty(code="W3",description="Wandern auf markierten Wanderpfaden, mit exponierten Stellen, die Trittsicherheit und Schwindelfreiheit erfordern, ausgesetzte Stellen können mit Seilen oder Ketten gesichert sein, eventuell braucht man die Hände fürs Gleichgewicht",sport_id="9"),
+Difficulty(code="B1",description="Begehen von markierten Steigen, Geröllflächen, weglosen Schrofen, Gelände bereits recht exponiert, erfordert Trittsicherheit und Schwindelfreiheit sowie Alpinklettern I. Schwierigkeitsgrad",sport_id="9"),
+Difficulty(code="B2",description="Begehen von markierten Steigen und weglosem Gelände, heikle Grashalden, steile Geröllflächen, einfache Firnfelder, erfordert Trittsicherheit und Schwindelfreiheit sowie Alpinklettern bis II. Schwierigkeitsgrad",sport_id="9"),
+Difficulty(code="B3",description="Begehen von meist weglosem Gelände, steilen Schrofen, Firnfelder mit Ausrutschgefahr, erfordert Trittsicherheit und Schwindelfreiheit sowie Alpinklettern über II. Schwierigkeitsgrad",sport_id="9")
 ])
 
 session.commit()
 
-session.add_all([User(name='Patrick', birthday="1991-05-17", male = True, height=180),
-User(name='Ania', birthday="1994-02-13", male = False, height = 158)])
+session.add_all([User(name="Patrick", birthday="1991-05-17", male = True, height=180),
+User(name="Ania", birthday="1994-02-13", male = False, height = 158)])
 
 session.commit()
 
@@ -60,5 +60,9 @@ RouteRun(name="Kleine Mehlmühle",location="Dorfen",distance = 3.19),
 RouteRun(name="Große Mehlmühle",location="Dorfen",distance = 3.84),
 RouteRun(name="kleine Seerunde",location="Krakau",distance = 2.87),
 RouteRun(name="Mangfall 6 Brücken",location="Rosenheim",distance = 6.95)])
+
+session.commit()
+
+session.add_all([MountainType(name="Pass"), MountainType(name="Gipfel"),MountainType(name="Vulkan")])
 
 session.commit()
