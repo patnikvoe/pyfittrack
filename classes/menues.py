0e652ae013f1a206b7ad950430f3320eef98e069
PatNikVoe <patrick.niklas.voelkl@gmial.com>
2018-11-07 12:17:09 +0100

    changed import to import from classes; changed to printTitle, printMenu, printPanda, invalidInput; Added mountaintype and users to show menu; Adjusted show.Difficulties, show.weight, show.sports, show.users

--- classes/menues.py
+++ classes/menues.py
@@ -1,18 +1,7 @@
-from sys import platform
+# import from plots
 from classes.plots import plot_goal_actuall
-from classes.functions import readTableFromDB, horizontalSeperator
-import pandas as pd
-import os
-from time import sleep
-from pyfittrack import clear
-from sqlalchemy import *
-from sqlalchemy import create_engine
-from sqlite3 import dbapi2 as sqlite
-import matplotlib.pyplot as plt
-import time
-import datetime
-from tabulate import tabulate
 
+# import classes
 from classes.classes import AlpineTrack
 from classes.classes import Country
 from classes.classes import Difficulty
@@ -24,9 +13,23 @@ from classes.classes import TrackRun
 from classes.classes import User
 from classes.classes import Weight
 
+#import database connection
+from classes import Base, Engine, session, Session
+
+#import from classes init
+from classes import descriptionWrap, pd, time, datetime, tabulate, plt, sleep
+
+#import custom functions from classes
+from classes.functions import *
+
+####################################################################################
+####################################################################################
+####################################################################################
+# Main Menu
 def menumain():
+    clear()
     print()
-    print("Main Menu")
+    printTitle("Main Menu")
     options = {"#": [1,
                      2,
                      3,
@@ -37,7 +40,7 @@ def menumain():
                           "Analysis Menu",
                           "-------------",
                           "Exit Programm"]}
-    print(tabulate(options,headers = "keys",tablefmt="psql",showindex="never"))
+    printMenu(options)
     while True:
         option = input("Option: ")
         try:
@@ -45,11 +48,9 @@ def menumain():
             if option <= len(options["#"])-2 and option >= 0:
                 break
             else:
-                horizontalSeperator(string = "!")
-                print("%s is an invalid Option. Try again!" % option)
+                invalidInput()
         except ValueError:
-            horizontalSeperator(string = "!")
-            print("%s is an invalid Option. Try again!" % option)
+            invalidInput()
     clear()
     print()
     if option == 1:
@@ -63,12 +64,18 @@ def menumain():
         print("Closing programm")
         horizontalSeperator()
         sleep(1)
+        clear()
         quit()
 
+####################################################################################
+####################################################################################
+####################################################################################
+# Add Menu
 def addMenu():
+    clear()
+    print()
     while True:
-        print()
-        print("Add Menu")
+        printTitle("Add Menu")
         options = {"#": [1,
                          2,
                          3,
@@ -89,7 +96,7 @@ def addMenu():
                               "Add new weight",
                               "---------------------",
                               "Back to main menu"]}
-        print(tabulate(options,headers = "keys",tablefmt="psql",showindex="never"))
+        printMenu(options)
         while True:
             option = input("Option: ")
             try:
@@ -97,55 +104,58 @@ def addMenu():
                 if option <= len(options["#"])-2 and option >= 0:
                     break
                 else:
-                    horizontalSeperator(string = "!")
-                    print("%s is an invalid Option. Try again!" % option)
+                    invalidInput()
             except ValueError:
-                horizontalSeperator(string = "!")
-                print("%s is an invalid Option. Try again!" % option)
+                invalidInput()
         clear()
         print()
         if option == 1:
             print("Add new running track")
             previousEntries = readTableFromDB(tb_tracksrun,engine, parse_dates=["DateDuration"])
             previousEntries = previousEntries.sort_values(by=["date_duration"])
-            print(tabulate(previousEntries[-5:],headers = "keys",tablefmt="psql"))
+            printPanda(previousEntries[-5:])
             addTrack(True)
         elif option == 2:
             print("Add new alpine track")
             previousEntries = readTableFromDB(tb_tracksalpine,engine,columns=["mountain_id", "sport_id", "date_duration", "summit","performancespeed"],parse_dates=["date_duration"])
             previousEntries = previousEntries.sort_values(by=["date_duration"])
-            print(tabulate(previousEntries[-5:],headers = "keys",tablefmt="psql"))
+            printPanda(previousEntries[-5:])
             addTrack(False)
         elif option == 3:
             print("Add new running route")
-            print(tabulate(readTableFromDB(tb_runroutes,engine),headers = "keys",tablefmt="psql"))
+            printPanda(readTableFromDB(tb_runroutes,engine))
             addRouteRunning()
         elif option == 4:
             print("Add new goals")
-            print(tabulate(readTableFromDB(tb_rungoals,engine,parse_dates=["date"]),headers = "keys",tablefmt="psql"))
+            printPanda(readTableFromDB(tb_rungoals,engine,parse_dates=["date"]))
         elif option == 5:
             print("Add new mountain")
-            print(tabulate(readTableFromDB(tb_mountains,engine),headers = "keys",tablefmt="psql"))
+            printPanda(readTableFromDB(tb_mountains,engine))
             addMountain()
         elif option == 6:
             print("Add new sport")
-            print(tabulate(readTableFromDB(tb_sport,engine),headers = "keys",tablefmt="psql"))
+            printPanda(readTableFromDB(tb_sport,engine))
             addSport()
         elif option == 7:
             print("Add new difficulty")
-            print(tabulate(readTableFromDB(tb_difficulty,engine),headers = "keys",tablefmt="psql"))
+            printPanda(readTableFromDB(tb_difficulty,engine))
             addDifficulty()
         elif option == 8:
             print("Add new weight")
-            print(tabulate(readTableFromDB(tb_weight,engine,parse_dates=["date"]),headers = "keys",tablefmt="psql"))
+            printPanda(readTableFromDB(tb_weight,engine,parse_dates=["date"]))
         elif option == 0:
             clear()
             break
 
+####################################################################################
+####################################################################################
+####################################################################################
+# Show Entries Menu
 def showTablesMenu():
+    clear()
+    print()
     while True:
-        print()
-        print("Show Data Menu")
+        printTitle("Show Data Menu")
         options = {"#": [1,
                          2,
                          3,
@@ -153,18 +163,22 @@ def showTablesMenu():
                          5,
                          6,
                          7,
+                         8,
+                         9,
                          "",
                          0],
                    "Option": ["Show running track",
-                              "Show alpine track",
                               "Show running route",
+                              "Show alpine track",
                               "Show mountain",
+                              "Show mountain types",
                               "Show sport",
                               "Show difficulty",
                               "Show weight",
+                              "Show users",
                               "------------------",
                               "Back to main menu"]}
-        print(tabulate(options,headers = "keys",tablefmt="psql",showindex="never"))
+        printMenu(options)
         while True:
             option = input("Option: ")
             try:
@@ -172,17 +186,15 @@ def showTablesMenu():
                 if option <= len(options["#"])-2 and option >= 0:
                     break
                 else:
-                    horizontalSeperator(string = "!")
-                    print("%s is an invalid Option. Try again!" % option)
+                    invalidInput()
             except ValueError:
-                horizontalSeperator(string = "!")
-                print("%s is an invalid Option. Try again!" % option)
+                invalidInput()
         clear()
         print()
         if option == 1:
-            print("Running Tracks:")
+            printTitle("Running Tracks")
             tracks = TrackRun.queryAll()
-            print(tabulate(tracks,headers = "keys",tablefmt="psql"))
+            printPanda(tracks)
 
             # # Calculate the meanspeed
             # meanspeed = tracks["speed"].mean()
@@ -191,7 +203,7 @@ def showTablesMenu():
             # means = {"mean of": ["pace","speed"],
             #          "value": [tracks["pace"].mean(), meanspeed],
             #          "units": ["min/km","km/h"]}
-            # print(tabulate(means,headers = "keys",tablefmt="psql"))
+            # printPanda(means)
 
             # Summing all times
             # runningTime = 0.0
@@ -203,43 +215,107 @@ def showTablesMenu():
             #
             # # calculating distance and display it
             # distance = {"Total Distance [km]": [runningTime*meanspeed]}
-            # print(tabulate(distance,headers = "keys",tablefmt="psql"))
+            # printPanda(distance)
 
+        ##################################################################################
+        # works as expected
         elif option == 2:
-            print("Alpine Tracks:")
-            tracks = AlpineTrack.queryAll()
-            print(tabulate(tracks,headers = "keys",tablefmt="psql"))
-        elif option == 3:
-            print("Running Routes:")
+            printTitle("Running Routes")
             routes = RouteRun.queryAll()
-            print(tabulate(routes,headers = "keys",tablefmt="psql"))
+            routes.columns = ["Name", "Location", "Distance"]
+            printPanda(routes)
+            print()
+        elif option == 3:
+            printTitle("Alpine Tracks")
+            tracks = AlpineTrack.queryAll()
+            printPanda(tracks)
         elif option == 4:
-            print("Mountains:")
+            printTitle("Mountains")
             mountains = Mountain.queryAll()
             mountains = mountains.sort_values(by=["name"])
-            print(tabulate(mountains,headers = "keys",tablefmt="psql"))
+            printPanda(mountains)
         elif option == 5:
-            print("Sports:")
-            sports = Sport.queryAll()
-            sports = sports.sort_values(by=["sport"])
-            print(tabulate(sports,headers = "keys",tablefmt="psql"))
+            printTitle("Mountain Types")
+            mountains = Mountain.queryAll()
+            mountains = mountains.sort_values(by=["name"])
+            printPanda(mountains)
         elif option == 6:
-            dif = Difficulty.queryAll()
-            print("Difficulties:")
-            print(tabulate(dif,headers = "keys",tablefmt="psql"))
+            printTitle("Sports")
+            sports = Sport.queryAll()
+            sports = sports.sort_values(by=["Sport"])
+            printPanda(sports)
+            print()
+        ##################################################################################
+        # works as expected
         elif option == 7:
-            print("Weight:")
-            weight = Weight.queryAll()
-            weight = weight.sort_values(by=["date"])
-            print(tabulate(weight, headers = "keys",tablefmt="psql"))
+            printTitle("Difficulties")
+            horizontalSeperator(length=descriptionWrap.width)
+            dif = Difficulty.queryAll()
+            dif = dif.sort_values(["sport_id","code"])
+            for index, row in dif.iterrows():
+                row["sport_id"] = Sport.selectName(row["sport_id"])
+                string = "| %s | Code: %s | ID: %d | Description:"  %(row["sport_id"], row["code"], index)
+                print(string)
+                horizontalSeperator(length=len(string)-15)
+                print("%s" %(descriptionWrap.fill(row["description"])))
+                horizontalSeperator(length=descriptionWrap.width)
+            print()
+        ##################################################################################
+        # works as expected
+        elif option == 8:
+            printTitle("Weight")
+            we = Weight.queryAll()
+            users = User.queryAll()
+            we["username"] = we.user_id.map(users.name.to_dict())
+            we["male"] = we.user_id.map(users.male.to_dict())
+            we = we.sort_values(["user_id","date"])
+            for index, row in we.iterrows():
+                # check if new User started and print out the Username if so
+                if index == 1:
+                    string = "{0}".format(we.at[index, "username"])
+                    printTitle(string)
+                    horizontalSeperatorWeight(we.at[index, "male"])
+                elif we.at[index, "username"] != we.at[index-1,"username"]:
+                    horizontalSeperatorWeight(we.at[index-1, "male"])
+                    string = "{0}".format(we.at[index, "username"])
+                    printTitle(string)
+                    horizontalSeperatorWeight(we.at[index, "male"])
+                # print out next user
+                if we.at[index, "male"]:
+                    string = "| {} | {:.1f} kg | Neck: {:.1f} cm | Waist: {:.1f} cm | Bodyfat: {:.1f} % |".format(row["date"].strftime("%Y-%m-%d"), row["weight"], row["neck"], row["waist"], row["bf"])
+                else:
+                    string = "| {} | {:.1f} kg | Neck: {:.1f} cm | Waist: {:.1f} cm | Hip: {:.1f} cm | Bodyfat: {:.1f} % |".format(row["date"].strftime("%Y-%m-%d"), row["weight"], row["neck"], row["waist"], row["hip"], row["bf"])
+                print(string)
+                if index == len(we.index):
+                    horizontalSeperatorWeight(we.at[index, "male"])
+            print()
+        ##################################################################################
+        # works as expected
+        elif option == 9:
+            printTitle("Users")
+            users = User.queryAll()
+            for index, row in users.iterrows():
+                string = ("| ID: {} | Name: {} | Birthday: {} | Male: {} | Height: {} cm |").format(index, row["name"], row["birthday"].strftime("%Y-%m-%d"), row["male"], row["height"])
+                if index == 1:
+                    horizontalSeperator(length =len(string)-2)
+                print(string)
+                horizontalSeperator(length =len(string)-2)
+            print()
+        ##################################################################################
+        # works as expected
         elif option == 0:
             clear()
             break
 
+####################################################################################
+####################################################################################
+####################################################################################
+# show Menu for analysis
 def analysisMenu():
+    clear()
+    print()
     while True:
-        print()
-        print("Analysis Menu")
+        printTitle("Analysis Menu")
         options = {"#": [1,
                          2,
                          3,
@@ -254,7 +330,7 @@ def analysisMenu():
                               "Weight Analysis",
                               "---------------------------",
                               "Back to main menu"]}
-        print(tabulate(options,headers = "keys",tablefmt="psql",showindex="never"))
+        printMenu(options)
         while True:
             option = input("Option: ")
             try:
@@ -262,25 +338,23 @@ def analysisMenu():
                 if option <= len(options["#"])-2 and option >= 0:
                     break
                 else:
-                    horizontalSeperator(string = "!")
-                    print("%s is an invalid Option. Try again!" % option)
+                    invalidInput()
             except ValueError:
-                horizontalSeperator(string = "!")
-                print("%s is an invalid Option. Try again!" % option)
+                invalidInput()
         clear()
         print()
         if option == 1:
-            print("Show Mountain with Summits")
+            printTitle("Show Mountain with Summits")
             #showMountainSummits(engine)
             #horizontalSeperator()
         elif option == 2:
-            print("Analysis for skitouring")
+            printTitle("Analysis for skitouring")
         elif option == 3:
-            print("Analysis for hiking")
+            printTitle("Analysis for hiking")
         elif option == 4:
-            print("Analysis for running")
+            printTitle("Analysis for running")
         elif option == 5:
-            print("Weight Analysis")
+            printTitle("Weight Analysis")
         elif option == 0:
             clear()
             break
