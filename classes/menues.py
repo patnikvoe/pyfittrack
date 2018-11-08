73a9f67b9dd95ee7384a8cbd2242ffaef623ea47
PatNikVoe <patrick.niklas.voelkl@gmial.com>
2018-11-08 09:40:15 +0100

    Removed engine from readTableFromDB calls; adjusted Show routes, mountains, mountain types, sports, differences, weight, users

--- classes/menues.py
+++ classes/menues.py
@@ -111,38 +111,38 @@ def addMenu():
         print()
         if option == 1:
             print("Add new running track")
-            previousEntries = readTableFromDB(tb_tracksrun,engine, parse_dates=["DateDuration"])
+            previousEntries = readTableFromDB(tb_tracksrun, parse_dates=["DateDuration"])
             previousEntries = previousEntries.sort_values(by=["date_duration"])
             printPanda(previousEntries[-5:])
             addTrack(True)
         elif option == 2:
             print("Add new alpine track")
-            previousEntries = readTableFromDB(tb_tracksalpine,engine,columns=["mountain_id", "sport_id", "date_duration", "summit","performancespeed"],parse_dates=["date_duration"])
+            previousEntries = readTableFromDB(tb_tracksalpine,columns=["mountain_id", "sport_id", "date_duration", "summit","performancespeed"],parse_dates=["date_duration"])
             previousEntries = previousEntries.sort_values(by=["date_duration"])
             printPanda(previousEntries[-5:])
             addTrack(False)
         elif option == 3:
             print("Add new running route")
-            printPanda(readTableFromDB(tb_runroutes,engine))
-            addRouteRunning()
+            printPanda(readTableFromDB(tb_runroutes))
+            #addRouteRunning()
         elif option == 4:
             print("Add new goals")
-            printPanda(readTableFromDB(tb_rungoals,engine,parse_dates=["date"]))
+            printPanda(readTableFromDB(tb_rungoals,parse_dates=["date"]))
         elif option == 5:
             print("Add new mountain")
-            printPanda(readTableFromDB(tb_mountains,engine))
-            addMountain()
+            printPanda(readTableFromDB(tb_mountains))
+            #addMountain()
         elif option == 6:
             print("Add new sport")
-            printPanda(readTableFromDB(tb_sport,engine))
-            addSport()
+            printPanda(readTableFromDB(tb_sport))
+            #addSport()
         elif option == 7:
             print("Add new difficulty")
-            printPanda(readTableFromDB(tb_difficulty,engine))
-            addDifficulty()
+            printPanda(readTableFromDB(tb_difficulty))
+            #addDifficulty()
         elif option == 8:
             print("Add new weight")
-            printPanda(readTableFromDB(tb_weight,engine,parse_dates=["date"]))
+            printPanda(readTableFromDB(tb_weight,parse_dates=["date"]))
         elif option == 0:
             clear()
             break
@@ -223,27 +223,112 @@ def showTablesMenu():
             printTitle("Running Routes")
             routes = RouteRun.queryAll()
             routes.columns = ["Name", "Location", "Distance"]
-            printPanda(routes)
+            routes = routes.sort_values(by=["Location","Name"])
+            routes = routes.reset_index(drop=True)
+            array = ["+"*routes.Name.map(len).max(), "+"*(routes.Distance.astype(str).map(len).max() +3)]
+            for index, row in routes.iterrows():
+                # check if new Location started and print out the routes in that Location if so
+                if index == 0:
+                    printTitle("{}".format(routes.at[index, "Location"]))
+                    horizontalSeperatorMulti(array)
+                elif routes.at[index, "Location"] != routes.at[index-1,"Location"]:
+                    horizontalSeperatorMulti(array)
+                    printTitle("{}".format(routes.at[index, "Location"]))
+                    horizontalSeperatorMulti(array)
+                # print out route
+                string = "| "
+                for column in routes.columns:
+                    if column == "Name":
+                        add = routes.at[index,"Name"]
+                        while len(add)< routes.Name.map(len).max():
+                            add += " "
+                        string += "{} | ".format(add)
+                    elif column == "Distance":
+                        add = str(routes.at[index,"Distance"])
+                        while len(add)< routes.Distance.astype(str).map(len).max()-2:
+                            add += " "
+                        string += "{} km | ".format(add)
+                print(string)
+                if index == len(routes.index)-1:
+                    horizontalSeperatorMulti(array)
             print()
         elif option == 3:
             printTitle("Alpine Tracks")
             tracks = AlpineTrack.queryAll()
             printPanda(tracks)
+        ##################################################################################
+        # works as expected
         elif option == 4:
             printTitle("Mountains")
             mountains = Mountain.queryAll()
-            mountains = mountains.sort_values(by=["name"])
-            printPanda(mountains)
+            mountains = mountains.drop(["mtype_id", "country_id"], axis=1)
+            mountains.columns = ["Name", "Range", "Elevation", "Type", "Country"]
+            mountains = mountains.sort_values(by=["Range","Name"])
+            array = ["+"*mountains.Name.map(len).max(), "+"*(mountains.Elevation.astype(str).map(len).max() +2), "+"*mountains.Type.map(len).max(), "+"*mountains.Country.map(len).max()]
+            mountains = mountains.reset_index(drop=True)
+            for index, row in mountains.iterrows():
+                if index == 0:
+                    printTitle("{}".format(mountains.at[index, "Range"]))
+                    horizontalSeperatorMulti(array)
+                elif mountains.at[index, "Range"] != mountains.at[index-1, "Range"]:
+                    horizontalSeperatorMulti(array)
+                    printTitle("{}".format(mountains.at[index, "Range"]))
+                    horizontalSeperatorMulti(array)
+                string = "| "
+                for column in mountains.columns:
+                    if column == "Name" or column =="Country" or column == "Type":
+                        add = mountains.at[index,column]
+                        while len(add)< mountains[column].map(len).max():
+                            add += " "
+                        string += "{} | ".format(add)
+                    elif column == "Elevation":
+                        add = str(mountains.at[index,"Elevation"])
+                        while len(add)< mountains.Elevation.astype(str).map(len).max()-2:
+                            add += " "
+                        string += "{} m | ".format(add)
+                print(string)
+                if index == len(mountains.index)-1:
+                    horizontalSeperatorMulti(array)
+            print()
+        ##################################################################################
+        # works as expected
         elif option == 5:
             printTitle("Mountain Types")
-            mountains = Mountain.queryAll()
-            mountains = mountains.sort_values(by=["name"])
-            printPanda(mountains)
+            mtype = MountainType.queryAll()
+            mtype = mtype.sort_values(by=["name"])
+            mtype.columns = ["Type"]
+            mtype = mtype.reset_index(drop=True)
+            maxLength = mtype.Type.map(len).max()
+            horizontalSeperator(length =maxLength+2)
+            for index, row in mtype.iterrows():
+                string = "%s" %row["Type"]
+                while len(string) < maxLength:
+                    string += " "
+                string = "| %s |" %string
+                print(string)
+                if index == len(mtype.index)-1:
+                    horizontalSeperator(length=len(string)-2)
+            print()
+        ##################################################################################
+        # works as expected
         elif option == 6:
             printTitle("Sports")
             sports = Sport.queryAll()
             sports = sports.sort_values(by=["Sport"])
-            printPanda(sports)
+            sports = sports.reset_index(drop=True)
+            maxLength = sports.Sport.map(len).max()
+            array = ["+"*sports.Sport.map(len).max()]
+            # print out route
+            for index, row in sports.iterrows():
+                if index == 0:
+                    horizontalSeperatorMulti(array)
+                add = sports.at[index,"Sport"]
+                while len(add)< sports.Sport.map(len).max():
+                    add += " "
+                string = "| {} | ".format(add)
+                print(string)
+                if index ==len(sports.index)-1:
+                    horizontalSeperatorMulti(array)
             print()
         ##################################################################################
         # works as expected
@@ -254,7 +339,7 @@ def showTablesMenu():
             dif = dif.sort_values(["sport_id","code"])
             for index, row in dif.iterrows():
                 row["sport_id"] = Sport.selectName(row["sport_id"])
-                string = "| %s | Code: %s | ID: %d | Description:"  %(row["sport_id"], row["code"], index)
+                string = "| %s | Code: %s | Description:"  %(row["sport_id"], row["code"])
                 print(string)
                 horizontalSeperator(length=len(string)-15)
                 print("%s" %(descriptionWrap.fill(row["description"])))
@@ -268,38 +353,48 @@ def showTablesMenu():
             users = User.queryAll()
             we["username"] = we.user_id.map(users.name.to_dict())
             we["male"] = we.user_id.map(users.male.to_dict())
-            we = we.sort_values(["user_id","date"])
+            we = we.sort_values(["username","date"])
             for index, row in we.iterrows():
                 # check if new User started and print out the Username if so
                 if index == 1:
-                    string = "{0}".format(we.at[index, "username"])
-                    printTitle(string)
-                    horizontalSeperatorWeight(we.at[index, "male"])
+                    printTitle("{0}".format(we.at[index, "username"]))
+                    string, array = stringArrayRowWeight(we.at[index, "male"], row)
+                    horizontalSeperatorMulti(array)
                 elif we.at[index, "username"] != we.at[index-1,"username"]:
-                    horizontalSeperatorWeight(we.at[index-1, "male"])
-                    string = "{0}".format(we.at[index, "username"])
-                    printTitle(string)
-                    horizontalSeperatorWeight(we.at[index, "male"])
+                    string, array = stringArrayRowWeight(we.at[index-1, "male"], row)
+                    horizontalSeperatorMulti(array)
+                    printTitle("{0}".format(we.at[index, "username"]))
+                    string, array = stringArrayRowWeight(we.at[index, "male"], row)
+                    horizontalSeperatorMulti(array)
                 # print out next user
-                if we.at[index, "male"]:
-                    string = "| {} | {:.1f} kg | Neck: {:.1f} cm | Waist: {:.1f} cm | Bodyfat: {:.1f} % |".format(row["date"].strftime("%Y-%m-%d"), row["weight"], row["neck"], row["waist"], row["bf"])
-                else:
-                    string = "| {} | {:.1f} kg | Neck: {:.1f} cm | Waist: {:.1f} cm | Hip: {:.1f} cm | Bodyfat: {:.1f} % |".format(row["date"].strftime("%Y-%m-%d"), row["weight"], row["neck"], row["waist"], row["hip"], row["bf"])
+                string, array = stringArrayRowWeight(we.at[index, "male"], row)
                 print(string)
                 if index == len(we.index):
-                    horizontalSeperatorWeight(we.at[index, "male"])
+                    horizontalSeperatorMulti(array)
             print()
         ##################################################################################
         # works as expected
         elif option == 9:
             printTitle("Users")
             users = User.queryAll()
+            users = users.sort_values(["name"])
+            maxNameLength = users.name.map(len).max()
+            array = ["Name: " + "+"*users.name.map(len).max(), "Birthday: " + "+"*10, "Male: " + "+"*5, "Height: " +"+"*3 + " cm"]
+            horizontalSeperatorMulti(array)
             for index, row in users.iterrows():
-                string = ("| ID: {} | Name: {} | Birthday: {} | Male: {} | Height: {} cm |").format(index, row["name"], row["birthday"].strftime("%Y-%m-%d"), row["male"], row["height"])
-                if index == 1:
-                    horizontalSeperator(length =len(string)-2)
+                string = ""
+                for column in users.columns:
+                    if users.columns.get_loc(column) == 0:
+                        string += "| Name: {:<{length}} |".format(row["name"],length=maxNameLength)
+                    elif users.columns.get_loc(column) == 1:
+                        string += " Birthday: {} |".format(row["birthday"].strftime("%Y-%m-%d"))
+                    elif users.columns.get_loc(column) == 2:
+                        string += " Male: {: <5} |".format(str(row["male"]))
+                    elif users.columns.get_loc(column) == 3:
+                        string +=" Height: {} cm |".format(row["height"])
                 print(string)
-                horizontalSeperator(length =len(string)-2)
+                if index == len(users.index)-1:
+                    horizontalSeperatorMulti(array)
             print()
         ##################################################################################
         # works as expected
